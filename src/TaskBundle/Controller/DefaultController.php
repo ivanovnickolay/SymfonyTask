9e23b9661ab19912b7a239629c02c432362d9510
Администратор <@10.0.0.240>
2015-08-24 23:22:19 +0300

    Базовые настройки. настройка подключений к БД

---
+++ src/TaskBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TaskBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TaskBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
